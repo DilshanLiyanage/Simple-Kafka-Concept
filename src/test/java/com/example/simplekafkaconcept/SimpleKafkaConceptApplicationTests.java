79ce6516bd818d055344d5b327a4ee0474148f76
TharushaGrubTech <tharusha.dilshan@grubtech.com>
2022-06-21 22:36:02 +0530

    first commit

---
+++ src/test/java/com/example/simplekafkaconcept/SimpleKafkaConceptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplekafkaconcept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleKafkaConceptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
